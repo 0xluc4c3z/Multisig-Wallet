89252a3401919b9fac0ee5a5597719cf4e66aa21
Lucacez <lucacespedes@protonmail.com>
2022-09-05 16:13:25 -0300

    test hardhat

--- test/MultisigWallet.test.ts
+++ test/MultisigWallet.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Token", function () {
+describe("MultisigWallet", function () {
   it("Should return name Token", async function () {
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy();
